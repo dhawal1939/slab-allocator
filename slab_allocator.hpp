64069e4d212d6c6cd4db4c15d07b5cd3e7d14f36
saptarshimanna <saptarshimanna@gmail.com>
2019-11-25 16:47:22 +0530

    first push

--- slab_allocator.hpp
+++ slab_allocator.hpp
@@ -5,19 +5,28 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-//#include 
 
-typedef struct slab_t;
+typedef struct slab_s;
 
-typedef struct kmem_cache_s
-{
-	slab_t *full;
-	slab_t *partial;
-	slab_t *free;
-	uint32_t object_size;
-	uint32_t number_object_size;
-	uint32_t total_number_objects;
-	uint32_t number_of_blocks_in_slab;
+typedef struct kmem_cache_s{
+	const char* name;
 
-};
+	slab_s* free_lst;
+	slab_s* partial_lst;
+	slab_s* full_lst;
+
+	uint obj_size;
+	uint num_obj_slab;
+	uint total_num_obj;
+	uint num_blocks_slab;
+
+	void(*ctor)(void*);
+	void(*dtor)(void*);
+
+	kmem_cache_s* next;
+	kmem_cache_s* prev;
+
+
+}kmem_cache_t;
+
 #endif //!SLAB_ALLOCATOR_H_
